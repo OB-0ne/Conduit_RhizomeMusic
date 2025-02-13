dcedd96d7a09bf23053204a8891028cab5b16831
OB-0ne <omkarbhatt8@gmail.com>
2025-02-14 02:42:54 +0530

    updated TURN server

--- static/receiver.js
+++ static/receiver.js
@@ -7,9 +7,14 @@ const rtcConfig = {
     iceServers: [
         { urls: ['stun:stun.l.google.com:19302','stun:stun1.l.google.com:19302'] },
         {
-            urls: "turn:relay.backups.cz?transport=tcp", // Free TURN server (temporary)
-            username: "webrtc",
-            credential: "webrtc"
+            urls: "turn:openrelay.metered.ca:80",
+            username: "openrelayproject",
+            credential: "openrelayproject"
+        },
+        {
+            urls: "turn:openrelay.metered.ca:443",
+            username: "openrelayproject",
+            credential: "openrelayproject"
         }
     ]
 };
